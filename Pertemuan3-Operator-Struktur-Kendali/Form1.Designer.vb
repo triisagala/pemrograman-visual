0da9ed70066bce2db31420f68555ec8fb0a3f162
Adeptri Sagala <adeptri58@gmail.com>
2026-09-01 13:31:09 +0700

    Pertemuan3-Struktur-Operator-Kendali

---
+++ Pertemuan3-Operator-Struktur-Kendali/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtNilai = New TextBox()
+        btnInput = New Button()
+        picImage = New PictureBox()
+        CType(picImage, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtNilai
+        ' 
+        txtNilai.Location = New Point(229, 178)
+        txtNilai.Name = "txtNilai"
+        txtNilai.Size = New Size(125, 27)
+        txtNilai.TabIndex = 0
+        ' 
+        ' btnInput
+        ' 
+        btnInput.Location = New Point(241, 222)
+        btnInput.Name = "btnInput"
+        btnInput.Size = New Size(94, 29)
+        btnInput.TabIndex = 1
+        btnInput.Text = "Input"
+        btnInput.UseVisualStyleBackColor = True
+        ' 
+        ' picImage
+        ' 
+        picImage.Location = New Point(241, 40)
+        picImage.Name = "picImage"
+        picImage.Size = New Size(102, 116)
+        picImage.SizeMode = PictureBoxSizeMode.Zoom
+        picImage.TabIndex = 2
+        picImage.TabStop = False
+        picImage.UseWaitCursor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(613, 450)
+        Controls.Add(picImage)
+        Controls.Add(btnInput)
+        Controls.Add(txtNilai)
+        Name = "Form1"
+        Text = "Form1"
+        CType(picImage, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtNilai As TextBox
+    Friend WithEvents btnInput As Button
+    Friend WithEvents picImage As PictureBox
+
+End Class
---
+++ Pertemuan3-Operator-Struktur-Kendali/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim nilai As Integer
+        If Not Integer.TryParse(txtNilai.Text, nilai) Then
+            MessageBox.Show("Masukkan dalam bentuk angka")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilai < 0 OrElse nilai > 100 Then
+            MessageBox.Show("Masukkan nilai 0-100")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilai <= 50 Then
+            picImage.Image = Image.FromFile("Assets\1.png")
+
+        ElseIf nilai <= 70 Then
+            picImage.Image = Image.FromFile("Assets\2.png")
+        Else
+            picImage.Image = Image.FromFile("Assets\3.png")
+        End If
+    End Sub
+
+    Private Sub txtNilai_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilai.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
+
+
